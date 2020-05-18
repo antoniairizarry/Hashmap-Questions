822ef071c7543d0f9259f5bd4340a806cfb90d94
Antonia <antonianomamiukor@gmail.com>
2020-05-18 00:41:31 -0700

    palindrome permutation tests passing

--- lib/palindrome_permutation.rb
+++ lib/palindrome_permutation.rb
@@ -1,4 +1,25 @@
 
 def palindrome_permutation?(string)
-  raise NotImplementedError, "palindrome_permutation? not implemented"
+  return true if string.empty?
+  
+  hash = {}
+  
+  string.split("").each do |letter|
+    if hash[letter].nil?
+      hash[letter] = 1
+    else
+      hash[letter] += 1
+    end
+  end
+  
+  odd = 0
+  
+  hash.values.each do |v|
+    odd += 1 if v.odd?
+  end
+  if odd <= 1
+    return true
+  else
+    return false
+  end
 end
