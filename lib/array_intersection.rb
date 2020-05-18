9d326f16bf5d6067c3150c167ceb06c0cdc2bc68
Antonia <antonianomamiukor@gmail.com>
2020-05-17 23:56:55 -0700

    array intersection tests passing

--- lib/array_intersection.rb
+++ lib/array_intersection.rb
@@ -1,3 +1,19 @@
 def intersection(list1, list2)
-  raise NotImplementedError, "Intersection not implemented"
+  return [] if list1 == nil || list2 == nil
+  
+  hash = {}
+  
+  list1.each do |element|
+    hash[element] = true
+  end
+  
+  result = []
+  
+  list2.each do |number|
+    if hash.has_key?(number)
+      result << number
+    end
+  end
+  result
+  
 end
